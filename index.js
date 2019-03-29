83bf3300bfafadf3f7fa0d37cf16d0b226188b27
alonsovargas3 <alonsovargas@gmail.com>
2019-03-29 11:35:37 -0700

    Updated code comments

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 process.env["NTBA_FIX_319"] = 1;
 require('dotenv').config();
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios');
@@ -16,30 +15,51 @@ const url = process.env.BASEURL;
 const port = process.env.PORT;
 const token = process.env.TOKEN;
 
-//<---------------- ZOBOT FUNCTIONALITY ---------------->
+//<---------------- EXPRESS ---------------->
 
-//Telegram module include and cretion of new bot instance
+// Calls the express function "express()" and puts new Express application
+// inside the app variable to start a new Express application
+const app = express();
+
+// Start Express server for our web application framework
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+
+// Passing bodyParser into Express
+// Returns middleware that will make sure we are only parsing JSON
+// bodyParser will make the form data available
+app.use(bodyParser.json());
+
+// This route will accept the HTTP POST payload sent by
+// Telegram whenever an user event has occurred
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+//<---------------- ZOBOT ---------------->
+
+// Telegram module include and cretion of new bot instance
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token);
 
 // This informs the Telegram servers of the new webhook.
 bot.setWebHook(`${url}/bot${token}`);
 
-const app = express();
-
-// parse the updates to JSON
-app.use(bodyParser.json());
-
-//Function to log what users arae saying
+// Conversation logging function
 function logChat(msg){
   console.log(msg.chat.username + " :: " + msg.text);
+  //TODO: log to database
 }
 
-//Function to send final message if something is not in the dictionary
+// Function to send final message if something is not in the dictionary
 function finalTry(chatId){
   bot.sendMessage(chatId,"Sorry, I didn't understand that. Type /help to see available commands.");
 }
 
+// Function returns the session state
+// Session state is stored in a database
 async function getSession(id){
   try{
 
@@ -55,26 +75,17 @@ async function getSession(id){
   }
 }
 
-// We are receiving updates at the route below!
-app.post(`/bot${token}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
-
-// Start Express Server
-app.listen(port, () => {
-  console.log(`Express server is listening on ${port}`);
-});
-
-
-// Matches /echo [whatever]
+// Matches /echo [whatever] and returns to use
+// This is a testing function that will allow developer to
+// understand how to use slash commands and variables after commands
 bot.onText(/\/echo (.+)/, function onEchoText(msg, match) {
   let resp = match[1];
   bot.sendMessage(msg.chat.id, resp);
   logChat(msg);
 });
 
-// Start message
+// Start message - this is what the user will first see
+// when initiating a session with the bot
 bot.onText(/\/start/, async function onStartText(msg) {
   let chatId = msg.chat.id;
   try {
@@ -92,19 +103,23 @@ bot.onText(/\/start/, async function onStartText(msg) {
 
 });
 
-// Start message
+// Settings - Functionality TBD
 bot.onText(/\/settings/, function onSettingsText(msg) {
   bot.sendMessage(msg.chat.id, 'Look deep inside...tell me what you find!');
   logChat(msg);
 });
 
-// Help message
+// Help - will return overview of bot and purpose
+// as well as commands and functionality
 bot.onText(/\/help/, function onHelpText(msg) {
   bot.sendMessage(msg.chat.id, 'Here are a list of comands \n /help \n /ojo \n /zobot \n /zobot [coin] <-- replace with the coin symbol such as btc or ltc and only enter one at a time');
   logChat(msg);
 });
 
-// Help message
+// Zobot message with nothing after the zobot command
+// This has no functionality
+// TODO: Update function so that this returns information about
+// zobot and how to use it
 bot.onText(/\/zobot/, function onZobotOnlyText(msg) {
   let zomsg = msg.text;
   if(zomsg=="/zobot"){
@@ -113,7 +128,10 @@ bot.onText(/\/zobot/, function onZobotOnlyText(msg) {
   }
 });
 
-// Cancel Calls
+// Cancel Calls - This cancels the flow of the current call adding process
+// in the event that the user does not want to add the call that was started
+// This also ensures that only one call is being added at a time - however
+// multiple calls can be added
 bot.onText(/\/cancel/, function onCancelText(msg) {
 
   const opts = {
@@ -143,7 +161,10 @@ bot.onText(/\/cancel/, function onCancelText(msg) {
   logChat(msg);
 });
 
-// Zobot
+// Ojo - Call adding system that initiates the ojo functionality
+// Users will be able to add calls or view their calls
+// TODO - Allow user to see all calls or top 10 calls
+// TODO - Allow user to lookup calls by user (/ojo @TheZoman)
 bot.onText(/\/ojo/, function onOjoText(msg) {
 
   const opts = {
@@ -174,7 +195,10 @@ bot.onText(/\/ojo/, function onOjoText(msg) {
 
 });
 
-// Zobot
+// Zobot - Allows user to lookup the Zobot status on a particular coin
+// Currently, Zobot supports Binance, Bittrex, and Poloniex
+// TODO: Return graceful message if the coin does not exist in the exchange
+// OR: Return only keyboard for supported exchanges for the coin
 bot.onText(/\/zobot (.+)/, function onZobotText(msg, match) {
   const chat_coin = match[1];
   const coin = chat_coin.toLowerCase();
@@ -216,7 +240,7 @@ bot.onText(/\/zobot (.+)/, function onZobotText(msg, match) {
 
 });
 
-// Handle callback queries
+// Handle callback queries - Callback queries are keyboard responses
 bot.on('callback_query', async function onCallbackQuery(callbackQuery) {
   const action = callbackQuery.data;
   const msg = callbackQuery.message;
@@ -331,8 +355,9 @@ bot.on('callback_query', async function onCallbackQuery(callbackQuery) {
 //Always checks messages to see if anaything needs to happen
 bot.on('message', async function onMessageQuery(msg) {
 
-  //Checks to see if message starts with /
-  //If it does, it ignores, otherwise attempts to process
+  // Checks to see if message starts with /
+  // If it does, it ignores, otherwise attempts to process
+  // Commands that start with / are handled above
   if(msg.text.substring(0,1)=="/"){
     return;
   } else {
@@ -346,33 +371,32 @@ bot.on('message', async function onMessageQuery(msg) {
         let next_step = resp.next_step;
         let id = resp.ID
 
+        // If there is an active session for the current user
         if(active_session == 1){
+
+          // CALL STEP - Coin Name
+          // Start by defining the coin for the call
           if(next_step=="coin_name" || next_step=="start"){
-            //log next step exchange
+            // Keyboard that asks user the exchange for the call
+            // Currently we support Binance, Bittrex, and Poloniex
             const opts = {
               reply_markup: {
                 inline_keyboard: [
                   [
                     {
                       text: 'Binance',
-                      // Check for this value when we listen
-                      // for "callback_query"
                       callback_data: 'binance_exchange'
                     }
                   ],
                   [
                     {
                       text: 'Bittrrex',
-                      // Check for this value when we listen
-                      // for "callback_query"
                       callback_data: 'bittrex_exchange'
                     }
                   ],
                   [
                     {
                       text: 'Poloniex',
-                      // Check for this value when we listen
-                      // for "callback_query"
                       callback_data: 'poloniex_exchange'
                     }
                   ],
@@ -384,24 +408,48 @@ bot.on('message', async function onMessageQuery(msg) {
             updateSession(msg.chat.id,"exchange","call",id);
             logChat(msg);
 
+          // CALL STEP - Target 1
+          // Define target 1 for the call
           } else if (next_step=="t1"){
             //log next step t2
             bot.sendMessage(msg.chat.id,"T1 recorded.");
             bot.sendMessage(msg.chat.id,"What is T2?");
             updateSession(msg.chat.id,"t2","call",id);
             logChat(msg);
+
+          // CALL STEP - Target 2
+          // Define target 2 for the call
           } else if (next_step=="t2"){
             //log next step t3
             bot.sendMessage(msg.chat.id,"T2 recorded.");
             bot.sendMessage(msg.chat.id,"What is T3?");
             updateSession(msg.chat.id,"t3","call",id);
+            const opts = {
+              reply_markup: {
+                inline_keyboard: [
+                  [
+                    {
+                      text: 'Finished',
+                      callback_data: 'finished_call'
+                      //make a case for this
+                    }
+                  ],
+                ]
+              }
+            }
             logChat(msg);
+
+          // CALL STEP - Target 3
+          // Define target 3 for the call
           } else if (next_step=="t3"){
             //log next step stop_loss
             bot.sendMessage(msg.chat.id,"T3 recorded.");
             bot.sendMessage(msg.chat.id,"Now I'll need your recommended stop loss?");
             updateSession(msg.chat.id,"stop_loss","call",id);
             logChat(msg);
+
+          // CALL STEP - Stop Loss
+          // Define stop loss for the call
           } else if (next_step=="stop_loss"){
             //log next step wrap_up
             //log date, type, user, status
@@ -430,6 +478,9 @@ bot.on('message', async function onMessageQuery(msg) {
             bot.sendMessage(msg.chat.id,"Ok, I have all the information I need. Please verify the following information and respond accordingly:",opts);
             updateSession(msg.chat.id,"wrap_up","call",id);
             logChat(msg);
+
+          // CALL STEP - Finish call process
+          // Confirm details of call with user 
           } else if (next_step=="wrap_up"){
             //return full call details
             //show keyboard with callback query zobot_call_complete
@@ -437,6 +488,7 @@ bot.on('message', async function onMessageQuery(msg) {
             updateSession(msg.chat.id,"complete","call",id);
             logChat(msg);
           }
+        // There is no current session for the user
         } else if(active_session == 0) {
           //###########COMPLETE THIS AREA###########
         }
